a0cbf8051d0573e5f17b35f9b9e4f6991e32b58f
sundq <tell88me@gmail.com>
2016-12-19 09:09:26 +0800

    add b tree

---
+++ b_tree.c
@@ -0,0 +1,176 @@
+#include "./b_tree.h"
+#include "./util/memory_pool.h"
+
+static inline b_tree_node_t *create_b_tree_node(b_tree *root)
+{
+	b_tree_node_t *node = (b_tree_node_t *) allocate_memory(sizeof (b_tree_node_t));
+	node->parent = NULL;
+	node->child = allocate_memory(sizeof (b_tree_node_t) * root->order);
+	node->key = allocate_memory(sizeof (int64_t) * root->order);
+	node->key_num = 0;
+}
+
+static inline int insert_key_to_tree_node(b_tree *btree, b_tree_node_t node, void *key)
+{
+	int i = 0;
+	for (i = 0; i < node->key_num; i++)
+	{
+		if (btree->compare(key, (void *) node->key[i]) < 0)
+		{
+			break; //node->key[i] = (int64_t) key;
+		}
+	}
+	for (int m = node->key_num; m < i; m--)
+	{
+		node->key[m] = node->key[m - 1];
+	}
+	node->key[i] = (int64_t) key;
+	node->key_num++;
+	return i;
+}
+
+//m为b tree的阶
+
+b_tree_t *b_tree_init(size_t m, compare_func_t func)
+{
+	b_tree_t *btree = (b_tree_t *) allocate_memory(sizeof (b_tree_t));
+	btree->compare = func;
+	btree->order = m;
+	btree->root = create_b_tree_node(btree);
+	return btree;
+}
+
+void *b_tree_find(b_tree_t *btree, void *key)
+{
+	int64_t ret = 0;
+	b_tree_node_t *current_node = btree->root;
+	while (current_node != NULL && ret == 0)
+	{
+		int i = 0;
+		for (i = 0; i < current_node->key_num; i++)
+		{
+			if (btree->compare(key, (void *) current_node->key[i]) < 0)
+			{
+				current_node = current_node->child[i];
+			}
+			else if (btree->compare(key, (void *) current_node->key[i]) == 0)
+			{
+				ret = current_node->key[i]; //find
+			}
+		}
+		current_node = current_node->child[i];
+	}
+	return (void *) ret;
+}
+
+int b_tree_add_node(b_tree_t *btree, void *key)
+{
+	int64_t ret = 0;
+	b_tree_node_t *current_node = btree->root;
+	while (1)
+	{
+		int i = 0;
+		for (i = 0; i < current_node->key_num; i++)
+		{
+			if (btree->compare(key, (void *) current_node->key[i]) < 0)
+			{
+				if (current_node->child[i] == NULL)
+				{
+					goto find_pos;
+				}
+				current_node = current_node->child[i];
+			}
+			else if (btree->compare(key, (void *) current_node->key[i]) == 0)
+			{
+				ret = current_node->key[i];
+			}
+		}
+		if (current_node->child[i] == NULL)
+		{
+			goto find_pos;
+		}
+		current_node = current_node->child[i];
+	}
+
+find_pos:
+	if (ret == 0)
+	{
+		insert_key_to_tree_node(btree, current_node, key);
+		while (current_node->key_num == btree->order) //分裂
+		{
+			int64_t middle_key = current_node->key[current_node->key_num / 2];
+			b_tree_node_t *parent = current_node->parent;
+			b_tree_node_t *new = create_b_tree_node(btree);
+			current_node->key_num = current_node->key_num / 2;
+			for (int i = current_node->key_num / 2 + 1, m = 0; i < current_node->key_num; i++, m++)
+			{
+				new->key[m] = current_node->key[i];
+				new->key_num++;
+			}
+			if (parent != NULL)
+			{
+				parent = create_b_tree_node(btree);
+				btree->root = parent;
+			}
+			new->parent = parent;
+			int index = insert_key_to_tree_node(btree, parent, (void *) middle_key);
+			for (int i = btree->order; i < index + 1; i--)
+			{
+				parent->child[i] = parent->child[i - 1];
+			}
+			parent->child[index] = current_node;
+			parent->child[index + 1] = new;
+			current_node = parent;
+		}
+	}
+	return 0;
+}
+
+int b_tree_del_node(b_tree_t *btree, void *key)
+{
+	int64_t ret = 0;
+	int index_node = 0;
+	b_tree_node_t *current_node = btree->root;
+	while (1)
+	{
+		for (index_node = 0; index_node < current_node->key_num; index_node++)
+		{
+			if (btree->compare(key, (void *) current_node->key[index_node]) < 0)
+			{
+				if (current_node->child[index_node] == NULL)
+				{
+					goto find_pos;
+				}
+				current_node = current_node->child[index_node];
+			}
+			else if (btree->compare(key, (void *) current_node->key[index_node]) == 0)
+			{
+				ret = current_node->key[index_node];
+			}
+		}
+		if (current_node->child[current_node->key_num] == NULL)
+		{
+			goto find_pos;
+		}
+		current_node = current_node->child[current_node->key_num];
+	}
+
+find_pos:
+	if (ret != 0)
+	{
+		if (current_node->is_leaf)
+		{
+			for (int i = index_node; i < current_node->key_num; i++)
+			{
+				current_node->key[i] = current_node->key[i + 1];
+			}
+			current_node->key_num--;
+		}
+	}
+
+	return 0;
+
+}
+
+
+
---
+++ b_tree.h
@@ -0,0 +1,51 @@
+/* 
+ * File:   b_tree.h
+ * Author: sundq
+ *
+ * Created on 2016年12月17日, 下午6:25
+ */
+
+#ifndef B_TREE_H
+#define	B_TREE_H
+
+#ifdef	__cplusplus
+extern "C"
+{
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <memory.h> 
+
+typedef struct b_tree_node b_tree_node_t;
+typedef struct b_tree b_tree_t;
+typedef int (* compare_func_t)(void *src, void *dst);
+
+struct b_tree_node
+{
+    size_t key_num; //实际关键字个数
+    struct b_tree_node *parent; //
+    struct b_tree_node *child;
+    int64_t *key; //关键字数组
+};
+
+struct b_tree
+{
+    struct b_tree_node *root;
+    size_t order;//b tree的阶
+    size_t node_num;
+    compare_func_t compare;
+};
+
+void *b_tree_find(b_tree_t *btree, void *key);
+int b_tree_add_node(b_tree_t *btree, void *key);
+int b_tree_del_node(b_tree_t *btree, void *key);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* B_TREE_H */
+
