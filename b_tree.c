a2a839663763445d9e6d10e4420b2b220b497f0b
sundq <tell88me@gmail.com>
2016-12-19 09:43:53 +0800

    add b tree

--- b_tree.c
+++ b_tree.c
@@ -65,64 +65,74 @@ void *b_tree_find(b_tree_t *btree, void *key)
 
 int b_tree_add_node(b_tree_t *btree, void *key)
 {
-	int64_t ret = 0;
 	b_tree_node_t *current_node = btree->root;
-	while (1)
+
+	//find the node which new key insert
+	int ret = 0;
+	while (ret == 0)
 	{
-		int i = 0;
-		for (i = 0; i < current_node->key_num; i++)
+		for (int i = 0; i < current_node->key_num; i++)
 		{
 			if (btree->compare(key, (void *) current_node->key[i]) < 0)
 			{
-				if (current_node->child[i] == NULL)
+				if (current_node->child[i] != NULL)
 				{
-					goto find_pos;
+					current_node = current_node->child[i];
 				}
-				current_node = current_node->child[i];
+				else
+				{
+					ret = 1;
+				}
+				break;
 			}
 			else if (btree->compare(key, (void *) current_node->key[i]) == 0)
 			{
-				ret = current_node->key[i];
+				goto ok; //find and return
 			}
 		}
-		if (current_node->child[i] == NULL)
+
+		if (current_node->child[current_node->key_num] != NULL)
 		{
-			goto find_pos;
+			current_node = current_node->child[current_node->key_num];
 		}
-		current_node = current_node->child[i];
+		else
+		{
+			break; //found
+		}
 	}
 
-find_pos:
-	if (ret == 0)
+	insert_key_to_tree_node(btree, current_node, key);
+	while (current_node->key_num == btree->order) //分裂
 	{
-		insert_key_to_tree_node(btree, current_node, key);
-		while (current_node->key_num == btree->order) //分裂
+		int64_t middle_key = current_node->key[current_node->key_num / 2];
+		b_tree_node_t *parent = current_node->parent;
+		b_tree_node_t *new = create_b_tree_node(btree);
+		for (int i = current_node->key_num / 2 + 1, m = 0; i <= current_node->key_num; i++, m++)
 		{
-			int64_t middle_key = current_node->key[current_node->key_num / 2];
-			b_tree_node_t *parent = current_node->parent;
-			b_tree_node_t *new = create_b_tree_node(btree);
-			current_node->key_num = current_node->key_num / 2;
-			for (int i = current_node->key_num / 2 + 1, m = 0; i < current_node->key_num; i++, m++)
-			{
-				new->key[m] = current_node->key[i];
-				new->key_num++;
-			}
-			if (parent != NULL)
-			{
-				parent = create_b_tree_node(btree);
-				btree->root = parent;
-			}
-			new->parent = parent;
-			int index = insert_key_to_tree_node(btree, parent, (void *) middle_key);
-			for (int i = btree->order; i < index + 1; i--)
-			{
-				parent->child[i] = parent->child[i - 1];
-			}
-			parent->child[index] = current_node;
-			parent->child[index + 1] = new;
-			current_node = parent;
+			new->key[m] = current_node->key[i];
+			new->key_num++;
+			current_node->key[i] = NULL;
+			current_node->child[i] = NULL;
 		}
+		current_node->key_num = current_node->key_num / 2;
+
+		if (parent != NULL)
+		{
+			parent = create_b_tree_node(btree);
+			btree->root = parent;
+		}
+		new->parent = parent;
+		int index = insert_key_to_tree_node(btree, parent, (void *) middle_key);
+		for (int i = btree->order; i < index + 1; i--)
+		{
+			parent->child[i] = parent->child[i - 1];
+		}
+		parent->child[index] = current_node;
+		parent->child[index + 1] = new;
+		current_node = parent;
 	}
+
+ok:
 	return 0;
 }
 
@@ -158,14 +168,12 @@ int b_tree_del_node(b_tree_t *btree, void *key)
 find_pos:
 	if (ret != 0)
 	{
-		if (current_node->is_leaf)
+
+		for (int i = index_node; i < current_node->key_num; i++)
 		{
-			for (int i = index_node; i < current_node->key_num; i++)
-			{
-				current_node->key[i] = current_node->key[i + 1];
-			}
-			current_node->key_num--;
+			current_node->key[i] = current_node->key[i + 1];
 		}
+		current_node->key_num--;
 	}
 
 	return 0;
